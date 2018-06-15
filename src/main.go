aaa246e2189c5a80211b0515c560b19de74366ca
tonnygoncalves <tonnygoncalves@gmail.com>
2018-06-14 22:26:31 -0300

    get api values

--- src/main.go
+++ src/main.go
@@ -3,18 +3,17 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"html"
 	"log"
 	"net/http"
 	"strconv"
 )
 
 var (
-	vTime          int     = 3600 //10 years
-	maxperimetro   float64 = 0
-	diamaximo      int     = 0
-	contadorSequia int64   = 0 // contador para saber cuantos días de sequía hay en el periodo a evaluar
-	contadorLluvia int64   = 0 // contador para saber cuantos días de lluvia hay en el periodo a evaluar
+	vTime          int //10 years
+	maxperimetro   float64
+	diamaximo      int
+	contadorSequia int64 // contador para saber cuantos días de sequía hay en el periodo a evaluar
+	contadorLluvia int64 // contador para saber cuantos días de lluvia hay en el periodo a evaluar
 )
 
 func main() {
@@ -27,33 +26,32 @@ func main() {
 	http.ListenAndServe(":9990", nil)
 }
 
+//handleGetData get clima router info
 func handleGetData(w http.ResponseWriter, r *http.Request) {
 
 	keys, ok := r.URL.Query()["dia"]
 
 	if !ok || len(keys) < 1 {
 		log.Println("Url Param 'dia' is missing")
+		w.Header().Set("Content-Type", "text/html")
+		w.Write([]byte("No ha colocado el parametro 'dia'!"))
 		return
 	}
 
-	if _, err := strconv.Atoi(""); err == nil {
-		fmt.Printf("%q looks like a number.\n", keys)
+	if _, err := strconv.Atoi(keys[0]); err != nil {
+		log.Println("%q - It looks like not a number.\n", keys[0])
+		w.Header().Set("Content-Type", "text/html")
+		w.Write([]byte("No ha ingresado un numero.\n"))
+		return
 	}
-
-	// Query()["key"] will return an array of items,
-	// we only want the single item.
 	key := keys[0]
-
-	log.Println("Url Param 'dia' is: " + string(key))
-	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-
 	type Day struct {
-		Dia    int
-		Tiempo string
+		dia   string
+		clima string
 	}
 	group := Day{
-		Dia:    1,
-		Tiempo: "Reds",
+		dia:   key,
+		clima: "Reds",
 	}
 
 	js, err := json.Marshal(group)
@@ -64,4 +62,8 @@ func handleGetData(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(js)
+}
+
+func process() {
+	vTime = 3600 //10 years
 }
